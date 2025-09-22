4bdb022dd045c6ae680acc442821c3d354d00c49
krevetkot <ksenia.slonimskaya@yandex.ru>
2025-09-22 14:12:32 +0300

    sum-digits-tail-rec is done

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -2,38 +2,30 @@
   (:gen-class))
 
 
-(def numbers-str ["111111111111111111111111111111111111111111111111111111111111111111"
-                  "111111111111111111111111111111111111111111111111111111111111111111"
-                  "111111111111111111111111111111111111111111111111111111111111111111"
-                  "111111111111111111111111111111111111111111111111111111111199999999"])
+(def numbers-str ["37107287533902102798797998220837590246510135740250"])
 
+; хвостовая рекурсия
+; в данном случае рекурсивно вызывается функция sum-column
 (defn sum-digits-tail-rec [nums]
   (letfn [(sum-column [cols carry result] ; функция принимает массив цифр текущего разряда из всех чисел, перенос и накопленный результат в обратном порядке
             (if (every? empty? cols) ; если цифры кончились
-              (if (zero? carry)
+              
+              (if (zero? carry) ; возвращаем либо результат, либо в начало результата кладем перенос
                 result
                 (cons carry result))
 
-              (let [current-digits (map #(if (empty? %) 0 (Character/digit (first %) 10)) cols)
-                    col-sum (+ carry (reduce + current-digits))
-                    digit (mod col-sum 10)
-                    new-carry (quot col-sum 10)
-                    next-cols (map rest cols)]
+              (let [current-digits (map #(if (empty? %) 0 (Character/digit (first %) 10)) cols) ; если цифры не кончились, каждую переводчим в число (если цифры нет - ноль)
+                    col-sum (+ carry (reduce + current-digits)) ; все складываем
+                    digit (mod col-sum 10) ; берет последнюю цифру суммы (эта цифра останется)
+                    new-carry (quot col-sum 10) ; берет все кроме последней цифры (перенос)
+                    next-cols (map rest cols)] ; убираем первый элемент во всех массивах(числах) 
                 (recur next-cols new-carry (cons digit result)))))]
-    (let [num-chars (map seq nums)
-          result (sum-column num-chars 0 '())]
-      (apply str (take 10 (reverse result))))))
-
-
-(defn sum-digits-with-map [nums]
-  (let [big-numbers (map biginteger nums)
-        total-sum (reduce + big-numbers)
-        sum-str (str total-sum)]
-    (subs sum-str 0 10)))
-
+    
+    (let [num-chars (map #(reverse (seq %)) nums) ; seq превращает строки в массивы с цифрами в обратном порыдке (чтобы складывать с младшего разряда)
+          result (sum-column num-chars 0 '())] 
+      (apply str (take 10 result)))))
 
 
 
 (defn -main [& args]
-  (println (sum-digits-tail-rec numbers-str))
-  (println (sum-digits-with-map numbers-str)))
+  (println (sum-digits-tail-rec numbers-str)))
