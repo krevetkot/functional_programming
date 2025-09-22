3d6c45c17986b9fc7df2eef08366f305f0a3bb44
krevetkot <ksenia.slonimskaya@yandex.ru>
2025-09-22 21:55:16 +0300

    3 functions in euler 17

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -1,6 +1,7 @@
 (ns lab1.core
   (:gen-class)
-  (:require [lab1.euler13 :as euler13]))
+  (:require [lab1.euler13 :as euler13]
+            [lab1.euler17 :as euler17]))
 
 
 
@@ -14,9 +15,12 @@
   (repeatedly 100 #(random-number 50))) ; 100 строк по 50 цифр
 
 
-
-
 (defn -main []
   (println "Euler 13")
-  (println "sum-digits-tail-rec:" (euler13/sum-digits-tail-rec numbers-str))
-  (println "sum-digits-with-map:" (euler13/sum-digits-with-map numbers-str)))
+  ;; (println "sum-digits-tail-rec:" (euler13/sum-digits-tail-rec numbers-str))
+  ;; (println "sum-digits-with-map:" (euler13/sum-digits-with-map numbers-str))
+
+  (println "\nEuler 17")
+  (println "count-letters-tail-rec:" (euler17/count-letters-tail-rec 1 1000))
+  (println "count-letters-rec:" (euler17/count-letters-rec 1 1000))
+  (println "count-letters-with-map:" (euler17/count-letters-with-map 1 1000)))
---
+++ lab1/src/lab1/euler17.clj
@@ -0,0 +1,55 @@
+(ns lab1.euler17
+  (:gen-class))
+
+(def number-words
+  {0 "" 1 "one" 2 "two" 3 "three" 4 "four" 5 "five"
+   6 "six" 7 "seven" 8 "eight" 9 "nine" 10 "ten"
+   11 "eleven" 12 "twelve" 13 "thirteen" 14 "fourteen" 15 "fifteen"
+   16 "sixteen" 17 "seventeen" 18 "eighteen" 19 "nineteen"
+   20 "twenty" 30 "thirty" 40 "forty" 50 "fifty"
+   60 "sixty" 70 "seventy" 80 "eighty" 90 "ninety"})
+
+(defn number-to-words [n]
+  (cond
+    (= n 1000) "one thousand"
+    (>= n 100) (let [hundreds (quot n 100)
+                     remainder (mod n 100)
+                     hundred-str (str (number-words hundreds) " hundred")]
+                 (if (zero? remainder)
+                   hundred-str
+                   (str hundred-str " and " (number-to-words remainder))))
+    (>= n 20) (let [tens (* (quot n 10) 10)
+                    units (mod n 10)
+                    tens-str (number-words tens)]
+                (if (zero? units)
+                  tens-str
+                  (str tens-str " " (number-words units))))
+    :else (number-words n)))
+
+
+(defn count-letters-tail-rec [start end]
+  (letfn [(count-iter [n total]
+            (if (> n end)
+              total
+              (let [words (number-to-words n) ; каждому числу сосставляем слова
+                    clean-words (clojure.string/replace words #"[ -]" "") ; удаляем пробелы 
+                    letter-count (count clean-words)] ; считаем буквы
+                (recur (inc n) (+ total letter-count)))))]
+    (count-iter start 0)))
+
+
+(defn count-letters-rec [n end]
+  (if (> n end)
+    0
+    (let [words (number-to-words n)
+          clean-words (clojure.string/replace words #"[ ]" "")
+          letter-count (count clean-words)]
+      (+ letter-count (count-letters-rec (inc n) end)))))
+
+
+(defn count-letters-with-map [start end]
+  (apply + (map (comp count
+                      #(clojure.string/replace % #"[ ]" "") number-to-words)
+                (range start (inc end)))))
+; пошагово: number-to-words к элементу, заменяем пробелы, считаем символы
+; все это собирается в композицию и применяется к каждому элементу. потом складывается
